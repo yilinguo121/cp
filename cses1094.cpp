225f26743b6b9211d4477c4378a3a32cb7d64ad6
Yi-Lin Guo <guovinn@gmail.com>
2024-07-10 18:19:22 +0800

    cses Increasing Array
    https://cses.fi/problemset/result/9812521/
    
    設一個vector，把這串陣列輸入進去，從v[1]開始看到v[n - 1]，
    比較v[i]有沒有比v[i - 1]大，如果沒有，答案就加上v[i - 1] - v[i]
    ，然後記得把v[i]改成v[i - 1]，不然下一個會算到舊的陣列，還有
    要開long long，不然答案會算錯。
    
    Signed-off-by: Yi-Lin Guo <guovinn@gmail.com>

---
+++ cses1094.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main (){
+	int n;
+	cin >> n;
+	vector<int> v;
+	for (int i = 0; i < n; i++){
+		int t;
+		cin >> t;
+		v.push_back(t);
+	}
+	int ans = 0;
+	for (int i = 1; i < v.size(); i++){
+		if (v[i] < v[i - 1]){
+			ans += v[i - 1] - v[i];
+			v[i] = v[i - 1];
+		}
+	}
+	cout << ans << '\n';
+}

1fc4316005b522cbd892014c4b7351f09a527dbb
Yi-Lin Guo <guovinn@gmail.com>
2024-07-31 16:21:18 +0800

    zj b606 Add All
    https://zerojudge.tw/ShowProblem?problemid=b606
    
    這段程式碼使用優先佇列（最小堆）來確保每次取出的數是
    當前最小的，從而最小化將一組數字相加的總成本。具體實
    現過程中，程式不斷地從優先佇列中取出最小的兩個數，計
    算其和，並將和重新放入優先佇列中，同時累加每次的和來
    計算總成本。當優先佇列中只剩下一個數時，總成本即為最
    小化的相加成本。此方法有效地降低了相加過程中的總成本
    ，時間複雜度為O(n log n)，適合處理大數據集。

--- b606.cpp
+++ b606.cpp
@@ -3,27 +3,25 @@ using namespace std;
 #define int long long
 signed main() {
 	int n;
-	ios_base::sync_with_stdio(0);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
 	while (cin >> n) {
 		if (n == 0) {
 			break;
 		}
-		deque<int> v(n);
+		priority_queue<int, vector<int>, greater<int>> v;
 		for (int i = 0;i < n;i++) {
-			cin >> v[i];
+			int t;
+			cin >> t;
+			v.push(t);
 		}
-		sort(v.begin(), v.end());
 		int ans = 0;
-		while (v.size() > 1) {
-			int sum = v[0];
-			v.pop_front();
-			sum += v[0];
-			v.pop_front();
-			ans += sum;
-			v.push_back(sum);
-			sort(v.begin(), v.end());
+		while(v.size() > 1) {
+			int a = 0;
+			a += v.top();
+			v.pop();
+			a += v.top();
+			v.pop();
+			v.push(a);
+			ans += a;
 		}
 		cout << ans << '\n';
 	}

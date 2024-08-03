25dcee32a9f92a597dfacf3eddddd93fc7468356
Yi-Lin Guo <guovinn@gmail.com>
2024-08-03 09:54:26 +0800

    zj d490 我也愛偶數
    https://zerojudge.tw/ShowProblem?problemid=d490
    
    這段程式碼讀取兩個整數a和b，計算並輸出從a到b（包含a和b）
    之間所有偶數的總和。使用了for迴圈遍歷區間內的每個數字，
    並通過條件判斷(i % 2 == 0)檢查該數是否為偶數，如果是偶
    數則將其加到變數ans 中。最後輸出計算結果 ans。
    
    Signed-off-by: Yi-Lin Guo <guovinn@gmail.com>

---
+++ d490.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int ans = 0;
+	for (int i = a;i <= b;i++) {
+		if (!(i % 2)) {
+			ans += i;
+		}
+	}
+	cout << ans;
+}
